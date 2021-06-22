c594f0f7caa186fee452b832aff259af71af3beb
xpedroleonardo <aispedroleonardo@gmail.com>
2021-06-22 16:48:41 -0300

    creating the entity

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,8 @@
+import 'reflect-metadata'
 import express from 'express'
 
+import './database'
+
 const app = express()
-
-/** 
- * GET = Buscar uma informação
- * POST = Criar uma informação
- * PUT = Alterar uma informação
- * DELETE = Apagar uma informação
- * PATCH = Alterar uma informação específica
-*/
-
-// Request(req)= Informações entrando
-// Response(res) = Informações saindo
-
-app.get('/g', (req, res) => {
-  return res.send("Esse é o método GET")
-})
-
-app.post('/p', (req, res) => {
-  return res.send("Esse é o método POST")
-})
-
 
 app.listen(3000, () => console.log("Server is running!"))
